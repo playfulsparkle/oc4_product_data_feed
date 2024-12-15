44d3ab0c925ebcf1655200b5f5dd17732f8238bf
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-12-15 08:47:32 +0100

    Türkçe language

---
+++ src/admin/language/tr-tr/feed/ps_google_base.php
@@ -0,0 +1,65 @@
+<?php
+// Heading
+$_['heading_title']                = 'Playful Sparkle - Google Base';
+$_['heading_categories']           = 'Kategori İlişkilendirme';
+$_['heading_authentication']       = 'Kimlik Doğrulama';
+$_['heading_tax_deffinitions']     = 'Vergi Tanımlaması';
+$_['heading_getting_started']      = 'Başlarken';
+$_['heading_setup']                = 'Google Base Kurulumu';
+$_['heading_troubleshot']          = 'Sorun Giderme';
+$_['heading_faq']                  = 'Sıkça Sorulan Sorular (SSS)';
+$_['heading_contact']              = 'Destek Ekibi';
+
+// Text
+$_['text_extension']               = 'Eklentiler';
+$_['text_success']                 = 'Başarılı: Google Base beslemeleri güncellendi!';
+$_['text_import_success']          = 'Başarılı: Google kategori listesini başarıyla içe aktardınız!';
+$_['text_add_category_success']    = 'Başarılı: Kategori ilişkilendirmesi tamamlandı!';
+$_['text_remove_category_success'] = 'Başarılı: Kategori ilişkilendirmesi kaldırıldı!';
+$_['text_edit']                    = 'Google Base Düzenle';
+$_['text_import']                  = 'Güncel Google kategorilerini <a href="https://support.google.com/merchants/answer/160081?hl=en" target="_blank" rel="external noopener noreferrer" class="alert-link">buradan</a> indirin. Numerik kategorileri (.txt uzantılı) yeşil alandan yükleyin.';
+$_['text_getting_started']         = '<p><strong>Genel Bakış:</strong> OpenCart 4.x kullanıcılarının, ürünlerini <a href="https://merchants.google.com" target="_blank" rel="external noopener noreferrer">Google Merchant Merkezi</a>\'ne XML formatında göndermelerini sağlar. Bu modül, Google Alışveriş listesinde ürün görünürlüğünü artırarak potansiyel müşteriler tarafından daha kolay keşfedilmesini ve satın alınmasını kolaylaştırır.</p><p><strong>Gereksinimler:</strong> Bu eklentiyi kullanmak için OpenCart 4.x+ ve PHP 7.4+ sürümlerinin yüklü ve yapılandırılmış olduğundan emin olun. Ayrıca, ürün listelerinizi etkili bir şekilde yönetmek için aktif bir Google Merchant hesabı gereklidir.</p>';
+$_['text_setup']                   = '<ul><li>Güncel Google kategorilerini <a href="https://support.google.com/merchants/answer/160081?hl=en" target="_blank" rel="external noopener noreferrer">Google Merchant Merkezi</a>\'nden indirin.</li><li>Mağazanızın kategorilerini Google kategorileri ile eşleştirin.</li><li>Gerekirse eklentiyi stokta olmayan ürünleri hariç tutacak şekilde yapılandırın.</li><li>Vergi tanımlarını kullanım ihtiyaçlarınıza uygun şekilde ayarlayın.</li></ul>';
+$_['text_troubleshot']             = '<ul><li><strong>Google kategorileri görüntülenmiyor:</strong> Google kategorilerinin en son listesini indirdiğinizden ve bunları sisteminize aktardığınızdan emin olun. Bu, web mağazanız ile Google Merchant Center arasında düzgün bir eşleme için önemlidir.</li><li><strong>Ürün beslemeleri görüntülenmiyor:</strong> Google Base eklentisinin OpenCart yönetici panelinizde etkin olduğunu doğrulayın. Etkinse ve hala herhangi bir çıktı görmüyorsanız, eklenti ayarlarında herhangi bir yanlış yapılandırma olup olmadığını kontrol edin.</li><li><strong>Ürünler Google Merchant sayfasında görünmüyor:</strong> Ürünlerinizin doğru şekilde kategorilendirildiğinden ve ürün verilerinizde hata olmadığından emin olun. Ürün listelemeleriyle ilgili Google Merchant politikalarına uyduğunuzdan emin olun.</li></ul>';
+$_['text_faq']                     = '<details><summary>Google Base modülü nedir?</summary><p>Google Base modülü, OpenCart 4.x kullanıcılarının ürünlerini Google Merchant Merkezi aracılığıyla Google Alışveriş sayfasında listelemesine yardımcı olur.</p></details><details><summary>Google Base modülünü nasıl aktif ederim?</summary><p>Eklentiyi OpenCart yönetici panelinde Eklentiler bölümünden etkinleştirebilirsiniz. Ayarları gerektiği gibi yapılandırdığınızdan emin olun.</p></details><details><summary>Ürün beslemelerini özelleştirebilir miyim?</summary><p>Evet, bu modül; vergi tanımları dahil olmak üzere çeşitli ayarları özelleştirmenize olanak tanır ve beslemenizin ihtiyaçlarınızı karşılamasını sağlar.</p></details><details><summary>Ürün beslemem neden Google Merchant Merkezi\'nde görünmüyor?</summary><p>Google Base uzantısının etkinleştirildiğinden ve ürün kategorilerinizin Google kategorileriyle doğru şekilde eşleştirildiğinden emin olun. Ayrıca, besleme yapılandırmanızda herhangi bir hata olup olmadığını kontrol edin.</p></details>';
+$_['text_contact']                 = '<p>Daha fazla yardım için lütfen destek ekibimizle iletişime geçin:</p><ul><li><strong>İletişim:</strong> <a href="mailto:%s">%s</a></li><li><strong>Döküman:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Kullanıcı Dökümanları</a></li></ul>';
+
+// Column
+$_['column_google_category']       = 'Google Kategorisi';
+$_['column_category']              = 'Kategori';
+$_['column_action']                = 'İşlem';
+
+// Tab
+$_['tab_general']                  = 'Genel';
+$_['tab_help_and_support']         = 'Yardım &amp; Destek';
+
+// Entry
+$_['entry_google_category']        = 'Google Kategorisi';
+$_['entry_category']               = 'Kategori';
+$_['entry_data_feed_url']          = 'Veri Akışı URL\'si';
+$_['entry_status']                 = 'Durum';
+$_['entry_login']                  = 'Kullanıcı Adı';
+$_['entry_password']               = 'Kullanıcı Şifresi';
+$_['entry_skip_out_of_stock']      = 'Stokta Olmayanları Gösterme';
+$_['entry_tax']                    = 'Vergi Öncesi Fiyatları Kullan';
+$_['entry_country']                = 'Ülke';
+$_['entry_region']                 = 'Bölge';
+$_['entry_tax_rate']               = 'Vergi Oranı';
+$_['entry_tax_ship']               = 'Nakliye Vergisi';
+$_['entry_active_store']           = 'Aktif Mağaza';
+$_['entry_additional_images']      = 'Ek Resimler Ekle';
+
+// Help
+$_['help_copy']                    = 'URL\'yi Kopyala';
+$_['help_open']                    = 'URL\'yi Aç';
+$_['help_additional_images']       = 'Bu seçeneği etkinleştirirseniz, Google Base beslemenize ek resimler eklenir. Bu ayarın etkinleştirilmesi besleme oluşturma sürecini yavaşlatabilir ve oluşturulan XML dosyasının boyutunu artırabilir.';
+
+// Error
+$_['error_permission']             = 'Uyarı: Google Base beslemesini değiştirme izniniz yok!';
+$_['error_store_id']               = 'Uyarı: Formda mağaza kimliği yok!';
+$_['error_currency']               = 'Uyarı: Listeden bir para birimi seçin';
+$_['error_upload']                 = 'Dosya yüklenemedi!';
+$_['error_filetype']               = 'Geçersiz dosya türü!';
+$_['error_tax_country']            = 'Vergi için ülke seçin.';
+$_['error_tax_region']             = 'Vergi bölgesi alanı boş bırakılamaz.';
+$_['error_tax_rate_id']            = 'Vergi için vergi oranı seçin';
